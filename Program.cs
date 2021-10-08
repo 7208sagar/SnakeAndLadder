7ba847211d762a5423c2824443abb2eefff63419
sagar <7208sk@gmail.com>
2021-10-08 23:57:28 +0530

    Rolling the die

--- Program.cs
+++ Program.cs
@@ -4,9 +4,25 @@ namespace SnakeAndLadder
 {
     class Program
     {
-        static void Main(string[] args)
+
+        const int START_POSTION = 0;
+        const int END_POSTION = 100;
+        public static int currentpoistion = 0;
+        public static int throwDice()
         {
-            Console.WriteLine("Hello World!");
+            //Console.WriteLine("Hello World!");
+
+            Random random = new Random();
+            int number = random.Next(1, 7);
+            return number;
+        }
+
+        static void Main(string[]args)
+        {
+            currentpoistion += throwDice();
+            Console.WriteLine("Current postion is=" + currentpoistion);
+            Console.WriteLine("Start postion is: " + START_POSTION);
+            Console.WriteLine("End postion is: " + END_POSTION);
         }
     }
 }
